ec181cdda70d8f55ec6b0f556d329bf433be3fb4
cx702qf@CX702QF <llopis.geoffrey@laposte.net>
2022-07-26 20:06:33 +0200

    Issue01

--- tests/unit_tests/test_login.py
+++ tests/unit_tests/test_login.py
@@ -1,7 +1,8 @@
+from flask import url_for
 
 def test_login_with_unknown_email(client, mock_clubs):
     email = "inexistant@email.com"
-    response = client.post("/showSummary", data={"email": email})
+    response = client.post("/showSummary", data={"email": email}, follow_redirects=True)
     expected_message = "Sorry, that email wasn&#39;t found."
     assert response.status_code == 200
     assert expected_message in response.data.decode()
@@ -9,7 +10,7 @@ def test_login_with_unknown_email(client, mock_clubs):
 
 def test_login_with_empty_email(client, mock_clubs):
     email = ""
-    response = client.post("/showSummary", data={"email": email})
+    response = client.post("/showSummary", data={"email": email}, follow_redirects=True)
     expected_message = "Sorry, you have to fill in an email."
     assert response.status_code == 200
     assert expected_message in response.data.decode()
@@ -18,6 +19,7 @@ def test_login_with_empty_email(client, mock_clubs):
 def test_login_with_a_known_email(client, mock_clubs):
     email = "test01@club.com"
     response = client.post("/showSummary", data={"email": email})
-    print(f"\nclient\n{client.__dict__}\n")
+    expected_message = "test01"
     assert response.status_code == 200
-    print(f"\nresponse\n{response.__dict__}\n")
+    assert expected_message in response.data.decode()
+
