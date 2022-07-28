786ea72c586baa4b8945e114ded3aa961939e5bd
cx702qf@CX702QF <llopis.geoffrey@laposte.net>
2022-07-28 20:41:23 +0200

    Issue 02

--- tests/unit_tests/test_maximun_points_allowed_by_clubs.py
+++ tests/unit_tests/test_maximun_points_allowed_by_clubs.py
@@ -21,34 +21,6 @@ def test_message_confirm_spending_points(client, mock_clubs, mock_competitions):
     assert response_purchase.status_code == 200
     assert expected_value in response_purchase.data.decode()
 
-    # response_login = login(client, mock_clubs)
-    #
-    # competition = mock_competitions[0]
-    # club = mock_clubs[0]
-    #
-    # print(f"\n{competition}\n\n{club}\n\n")
-    #
-    # response_book = client.post(
-    #     f"/book/{competition['name']}/{club['name']}",
-    #     data={"club": club, "competition": competition, "places": 6}
-    # )
-    #
-    # response_purchase_places = client.post(
-    #     "/purchasePlaces",
-    #     data={
-    #         "club": club,
-    #         "competition": competition,
-    #         "places": 6
-    #     }
-    # )
-    # print(f"\npurchase\n{response_purchase_places}\n")
-    # expected_message = "Great-booking complete!"
-    #
-    # assert response_book.status_code == 200
-    # assert response_purchase_places == 200
-    # assert response_book.redirect() == client.post("/purchasePlaces")
-    # assert expected_message == response_purchase_places.data.decode()
-    # pass
 
 
 def test_correct_deduction_of_spent_points(client, mock_clubs):
