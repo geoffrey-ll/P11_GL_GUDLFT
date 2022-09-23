02d224a5fd03c8c1b880612fc658ed579433bc17
cx702qf@CX702QF <llopis.geoffrey@laposte.net>
2022-09-23 05:01:42 +0200

    update locustfile.py

--- tests/performance_tests/locustfile.py
+++ tests/performance_tests/locustfile.py
@@ -1,40 +1,101 @@
 from locust import HttpUser, task
 from flask import url_for
-from unittest import mock
-
-# from ..conftest import mock_filename_clubs
-# import server
-
-
-# server.FILENAME_CLUBS = "./tests/clubs_test.json"
-
 
 # on_start()    on_stop()
 
-# @mock.patch("server.FILENAME_CLUBS", "tests/conftest.FILENAME_CLUBS_TEST")
-class GudlftPerfTest(HttpUser):#mock_filename_clubs):
+class GudlftPerfTest(HttpUser):
+    MESSAGE_TOO_LONG_RESPONSE = ""
 
-    def test_bal(self):
-        print("ok")
-        # print(f"\n{url_for('index')}\n")
-        pass
-
-    @task#(6)
-    def task_access_index_page(self):
-        self.test_bal()
-        self.client.get("/")
-        # self.client.get(url_for("index"))
+    def test(self, method, path, time_over, data_test=None):
+        if method == "GET":
+            with self.client.get(f"{path}", catch_response=True) as response:
+                if response.elapsed.total_seconds() > time_over:
+                    response.failure(self.MESSAGE_TOO_LONG_RESPONSE)
+        elif method == "POST":
+            with self.client.post(f"{path}", data=data_test,
+                                  catch_response=True) as response:
+                if response.elapsed.total_seconds() > time_over:
+                    response.failure(self.MESSAGE_TOO_LONG_RESPONSE)
 
     @task
-    # Chargement < 5s
-    def task_loading_time_show_summary(self):
+    def index(self):
+        self.test("GET", "/", 5)
+
+    @task
+    def show_summary(self):
         data_test = {"email": "john@simplylift.co"}
-        data_test_test = {"email": "test01@club.com"}
-        # self.client.post(url_for("showSummary"), data=data_test)
-        self.client.post("/showSummary", data_test)
-        self.client.post("/showSummary", data_test_test)
+        self.test("POST", "/showSummary", 5, data_test=data_test)
 
     @task
-    # Update json < 2s.
-    def task_writing_after_purchase_place(self):
-        pass
+    def book(self):
+        self.test("GET", "/book/Spring Festival/Simply Lift", 5)
+
+    @task
+    def purchase_places(self):
+        data_test = {
+            "club": "Simply Lift",
+            "competition": "Fall Classic",
+            "places": 1
+        }
+        self.test("POST", "/purchasePlaces", 2, data_test)
+
+    @task
+    def points_boards(self):
+        self.test("GET", "/pointsBoard", 5)
+
+    @task
+    def logout(self):
+        self.test("GET", "/logout", 5)
+
+    # @task#(6)
+    # def index(self):
+    #     print(f"\nTEST\n{GudlftPerfTest.index.__name__}\n")
+    #     with self.client.get('/', catch_response=True) as response:
+    #         if response.elapsed.total_seconds() > 5:
+    #             response.failure(self.MESSAGE_TOO_LONG_RESPONSE)
+    #     # Si pas de response ?
+    #
+    # @task
+    # # Chargement < 5s
+    # def show_summary(self):
+    #     data_test = {"email": "john@simplylift.co"}
+    #     with self.client.post(
+    #             "/showSummary", data=data_test,
+    #              catch_response=True) as response:
+    #         if response.elapsed.total_seconds() > 5:
+    #             response.failure(self.MESSAGE_TOO_LONG_RESPONSE)
+    #
+    # @task
+    # def book(self):
+    #     with self.client.get(
+    #             "/book/Spring Festival/Simply Lift",
+    #             catch_response=True) as response:
+    #         if response.elapsed.total_seconds() > 5:
+    #             response.failure(self.MESSAGE_TOO_LONG_RESPONSE)
+    #
+    #
+    # @task
+    # # Update json < 2s.
+    # def purchase_place(self):
+    #     data_test = {
+    #         "club": "Simply Lift",
+    #         "competition": "Fall Classic",
+    #         "places": 1
+    #     }
+    #     with self.client.post(
+    #             "/purchasePlaces", data=data_test,
+    #             catch_response=True) as response:
+    #         if response.elapsed.total_seconds() > 2:
+    #             response.failure(self.MESSAGE_TOO_LONG_RESPONSE)
+    #
+    # @task
+    # def points_board(self):
+    #     with self.client.get("/pointsBoard", catch_response=True) as response:
+    #         if response.elapsed.total_seconds() > 5:
+    #             response.failure(self.MESSAGE_TOO_LONG_RESPONSE)
+    #
+    # @task
+    # def logout(self):
+    #     with self.client.get("/logout", catch_response=True) as response:
+    #         if response.elapsed.total_seconds() > 5:
+    #             response.failure(self.MESSAGE_TOO_LONG_RESPONSE)
