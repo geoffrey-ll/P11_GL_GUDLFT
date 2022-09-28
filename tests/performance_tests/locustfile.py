aaab4c96d211fba5e92bc0e564d6bbffcede9b32
cx702qf@CX702QF <llopis.geoffrey@laposte.net>
2022-09-28 16:17:37 +0200

    Tests de performances

--- tests/performance_tests/locustfile.py
+++ tests/performance_tests/locustfile.py
@@ -1,34 +1,32 @@
 from locust import HttpUser, task
-from flask import url_for
 
-# on_start()    on_stop()
 
 class GudlftPerfTest(HttpUser):
-    MESSAGE_TOO_LONG_RESPONSE = ""
+    MESSAGE_TOO_LONG_RESPONSE = "The request takes too long."
 
-    def test(self, method, path, time_over, data_test=None):
-        if method == "GET":
-            with self.client.get(f"{path}", catch_response=True) as response:
-                if response.elapsed.total_seconds() > time_over:
-                    response.failure(self.MESSAGE_TOO_LONG_RESPONSE)
-        elif method == "POST":
-            with self.client.post(f"{path}", data=data_test,
-                                  catch_response=True) as response:
-                if response.elapsed.total_seconds() > time_over:
-                    response.failure(self.MESSAGE_TOO_LONG_RESPONSE)
+    def perf_get_request(self, path, time_over):
+        with self.client.get(f"{path}", catch_response=True) as response:
+            if response.elapsed.total_seconds() > time_over:
+                response.failure(self.MESSAGE_TOO_LONG_RESPONSE)
+
+    def perf_post_request(self, path, time_over, data_test):
+        with self.client.post(f"{path}", data=data_test,
+                              catch_response=True) as response:
+            if response.elapsed.total_seconds() > time_over:
+                response.failure(self.MESSAGE_TOO_LONG_RESPONSE)
 
     @task
     def index(self):
-        self.test("GET", "/", 5)
+        self.perf_get_request("/", 5)
 
     @task
     def show_summary(self):
         data_test = {"email": "john@simplylift.co"}
-        self.test("POST", "/showSummary", 5, data_test=data_test)
+        self.perf_post_request("/showSummary", 5, data_test=data_test)
 
     @task
     def book(self):
-        self.test("GET", "/book/Spring Festival/Simply Lift", 5)
+        self.perf_get_request("/book/Spring Festival/Simply Lift", 5)
 
     @task
     def purchase_places(self):
@@ -37,12 +35,12 @@ class GudlftPerfTest(HttpUser):
             "competition": "Fall Classic",
             "places": 1
         }
-        self.test("POST", "/purchasePlaces", 2, data_test)
+        self.perf_post_request("/purchasePlaces", 2, data_test)
 
     @task
     def points_boards(self):
-        self.test("GET", "/pointsBoard", 5)
+        self.perf_get_request("/pointsBoard", 5)
 
     @task
     def logout(self):
-        self.test("GET", "/logout", 5)
+        self.perf_get_request("/logout", 5)
